94b16972b089b3fa9be0a46d6acdd8d6509083da
LayalHF <44494720+LayalHF@users.noreply.github.com>
2026-01-13 13:10:25 +0100

    add tailwind css

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,11 +1,10 @@
 export default function GameOver({ winner, onRestart }) {
 
     return (<div id="game-over">
-        <h2>Game Over!</h2>
-        {winner && <p>{winner} won!</p>}
-        {!winner && <p>It's a Draw!</p>}
+        {winner && <h2>{winner} won!</h2>}
+        {!winner && <h2>It's a Draw!</h2>}
         <p>
-            <button onClick={onRestart}>Rematch!</button>
+            <button className="mt-15" onClick={onRestart}>Rematch!</button>
         </p>
     </div>);
 
